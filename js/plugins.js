9f85a2dd2e65ec7db6db02deec545d8dbe37c772
scottyyyyy <32375007+scottyyyyy@users.noreply.github.com>
2020-03-03 18:56:58 +0000

    tidy up

--- js/plugins.js
+++ js/plugins.js
@@ -12,7 +12,6 @@ function checkScroll() {
   }
 }
 
-$('#curved-name').circleType({radius:135});
 
 /*========== ADD SOLID CLASS TO NAVBAR WHEN TOGGLED ==========*/
 $('.navbar-toggler').click(function () {
@@ -44,22 +43,7 @@ $(document).ready(function () {
   });
 });
 
-/*========== LIGHTBOX IMAGE GALLERY ==========*/
 
-
-/*========== MEET THE TEAM CAROUSEL ==========*/
-
-
-/*========== SKILLS COUNTER ==========*/
-
-
-/*========== CLIENTS CAROUSEL ==========*/
-
-
-/*========== TOP SCROLL BUTTON ==========*/
-
-
-/*========== MAKE ALL ANIMATION "FADEINUP" ON MOBILE ==========*/
 
 
 
@@ -91,52 +75,4 @@ $(function () { // a self calling function
 
   onScrollInit($('.os-animation')); //function call with only items
   onScrollInit($('.staggered-animation'), $('.staggered-animation-container')); //function call with items and trigger
-});
-
-
-/*========== CONTACT FORM INPUT VALIDATION ==========*/
-//Original Resource: https://bootstrapious.com/p/how-to-build-a-working-bootstrap-contact-form
-$(function () {
-
-  // init the validator
-  // validator files are included in the download package
-  // otherwise download from http://1000hz.github.io/bootstrap-validator
-
-  $('#contact-form').validator();
-
-
-  // when the form is submitted
-  $('#contact-form').on('submit', function (e) {
-
-      // if the validator does not prevent form submit
-      if (!e.isDefaultPrevented()) {
-          var url = "contact/contact.php";
-
-          // POST values in the background the the script URL
-          $.ajax({
-              type: "POST",
-              url: url,
-              data: $(this).serialize(),
-              success: function (data) {
-                  // data = JSON object that contact.php returns
-
-                  // we recieve the type of the message: success x danger and apply it to the
-                  var messageAlert = 'alert-' + data.type;
-                  var messageText = data.message;
-
-                  // let's compose Bootstrap alert box HTML
-                  var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-
-                  // If we have messageAlert and messageText
-                  if (messageAlert && messageText) {
-                      // inject the alert to .messages div in our form
-                      $('#contact-form').find('.messages').html(alertBox);
-                      // empty the form
-                      $('#contact-form')[0].reset();
-                  }
-              }
-          });
-          return false;
-      }
-  })
 });
